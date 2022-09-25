851eb092caf09d3043d7ed78c0754240eb6366cf
Jyotirani12 <jyoti.lohan@gmail.com>
2022-09-25 19:46:52 +0530

    third

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/zomato')
+//mongoose.connect('mongodb://localhost:27017/zomato')
+mongoose.connect('mongodb+srv://test:test123@cluster0.cxbxf.mongodb.net/?retryWrites=true&w=majority')
